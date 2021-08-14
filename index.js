f51f3caa2c8afa703d0af79b38873015008a0d14
Andrews Mensah <andrews.mensah@amalitech.org>
2021-08-14 23:34:26 +0000

    ejs files updated

--- index.js
+++ index.js
@@ -11,9 +11,42 @@ const PORT = process.env.PORT || 7000;
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({
-  extended: true
+  extended: false
 }));
+
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+//Rendering homepage
+app.get('/', (req,res)=> {
+    res.render("index");
+});
+
+//Rendering admin login page
+app.get('/admin/login', (req,res)=>{
+    res.render("adminlogin")
+});
+
+
+//Rendering admin dashboard page
+app.get('/admin/dashboard', (req,res)=>{
+    res.render("admindashboard")
+});
+
+app.get('/admin/logout', (req,res)=> {
+    req.logout();
+    res.redirect('/admin/login')
+})
+
+
+//Rendering users dashboard page
+app.get('/users/dashboard', (req,res)=>{
+    res.render("userDashboard")
+});
+
+
+
+
 
 
 
