07a6b26f1ce59edc9913a8f88dded3bb6d7145c1
Andrews Mensah <andrews.mensah@amalitech.org>
2021-08-14 23:09:40 +0000

    port number added

--- index.js
+++ index.js
@@ -6,9 +6,34 @@ const bcrypt = require('bcrypt');
 
 const app = express();
 
+//Assigning a port number
+const PORT = process.env.PORT || 4000;
+
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+
+
+
+
+
+
+
+//Start the server
+app.listen(PORT, ()=>{
+    console.log(`My server is running on port ${PORT}`)
+});
+
+
+
+
+
+
+
+
 
